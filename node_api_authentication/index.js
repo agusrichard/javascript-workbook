20590ec202db5e372c932c5e33228dc84f7becd5
agusrichard <agus.richard21@gmail.com>
2020-02-28 18:04:31 +0700

    Add user validation

--- node_api_authentication/index.js
+++ node_api_authentication/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Init app
 const app = express();
+
+// Env config
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true },
+  () => console.log('Connected to db!')
+);
 
 
 // Import route
 const authRoute = require('./routes/auth.js');
 
+// Middleware
+app.use(express.json());
 
 // Routes middleware
 app.use('/api/user', authRoute);
