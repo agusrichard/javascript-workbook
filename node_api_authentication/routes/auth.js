2226216aa6f66a72a0426b2d7e218a587ca87e8a
agusrichard <agus.richard21@gmail.com>
2020-02-29 15:53:09 +0700

    Add register and login validation

--- node_api_authentication/routes/auth.js
+++ node_api_authentication/routes/auth.js
@@ -1,32 +1,42 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+const { registerValidation } = require('../validation');
 
-const Joi = require('@hapi/joi');
-
-const schema = Joi.object({
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required()
-})
 
 router.post('/register', async (req, res) => {
 
   // VALIDATE 
-  const validation = schema.validate(req.body);
-  res.send(validation);
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-  // const user = new User({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   password: req.body.password
-  // });
+  console.log('There is no error');
+
+  // Checking if the user is already in the database
+  // const emailExist = await User.findOne({email: req.body.email})
+  // if (emailExist) return res.status(400).send('Email already exists');
+
+  console.log('No clashed of email');
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  // CREATE NEW USER
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
+  });
+
+  console.log(user);
   
-  // try {
-  //   const savedUser = await user.save();
-  //   res.send(savedUser);
-  // } catch(err) {
-  //     res.status(400).send(err);
-  // }
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch(err) {
+      res.status(400).send(err);
+  }
 });
 
 module.exports = router;
---
+++ node_api_authentication/validation.js
@@ -0,0 +1,24 @@
+const Joi = require('@hapi/joi');
+
+// Register validation
+const registerValidation = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+  })
+
+  return schema.validate(data);
+}
+
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+  })
+
+  return schema.validate(data);
+}
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
