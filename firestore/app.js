d989e9e3684f62cb62fed16d1f597e96f9b8e4a7
agusrichard <agus.richard21@gmail.com>
2020-06-03 08:44:54 +0700

    Creating data

--- firestore/app.js
+++ firestore/app.js
@@ -1,17 +1,18 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form')
 
 function renderCafe(doc) {
   let li = document.createElement('li');
   let name = document.createElement('span');
   let city = document.createElement('span');
 
-  li.setAttribute('data-id', doc.id)
-  name.textContent = doc.data().name
-  city.textContent = doc.data().city
+  li.setAttribute('data-id', doc.id);
+  name.textContent = doc.data().name;
+  city.textContent = doc.data().city;
   
-  li.appendChild(name)
-  li.appendChild(city)
-  cafeList.appendChild(li)
+  li.appendChild(name);
+  li.appendChild(city);
+  cafeList.appendChild(li);
 }
 
 db.collection('cafes').get().then((snapshot) => {
@@ -19,3 +20,16 @@ db.collection('cafes').get().then((snapshot) => {
     renderCafe(doc)
   });
 });
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  db.collection('cafes').add({
+    name: form.name.value,
+    city: form.city.value
+  });
+
+  form.name.value = '';
+  form.city.value = '';
+
+})
