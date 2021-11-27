a4c720acee7a46d9f626c63049b883d3440f2d62
agusrichard <agus.richard21@gmail.com>
2021-11-27 15:51:14 +0700

    Experiment even further

--- express-one/index.js
+++ express-one/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.status(400).send('Hello World!')
+    res.status(300).send('Hello World!')
 })
 
 app.listen(3000)
